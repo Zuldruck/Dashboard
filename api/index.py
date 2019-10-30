3b9d44d1ccdc8f00161fe3134171643967b20f38
Lucas <lucas.sanchez@epitech.eu>
2019-10-30 09:22:02 +0100

    feat(master): backend server setup

---
+++ api/index.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import jsonify
+import pyrebase
+import json
+import time
+
+app = Flask(__name__)
+
+config = {
+  "apiKey": "AIzaSyAT3jR4FM2Aj-V90sYfVpOv2O96j2H6x5Y",
+  "authDomain": "dashboard-d7ebf.firebaseapp.com",
+  "databaseURL": "https://dashboard-d7ebf.firebaseio.com",
+  "storageBucket": "dashboard-d7ebf.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password("lucas.sanchez@epitech.eu", "dashboard1234")
+
+@app.route('/')
+def root():
+    users = db.child('users').get(user['idToken'])
+    return jsonify(users.val())
+
+@app.route('/about')
+def about():
+    file = open("../about.json", "r")
+    content = file.read()
+    about_json = json.loads(content)
+    about_json["server"]["current_time"] = int(time.time())
+    return jsonify(about_json)
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/Users.js
@@ -0,0 +1,23 @@
+// in src/users.js
+import React from 'react';
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+function UserList(props) {
+    console.log(props);
+    return(
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="name" />
+                <TextField source="username" />
+                <EmailField source="email" />
+                <TextField source="address.street" />
+                <TextField source="phone" />
+                <TextField source="website" />
+                <TextField source="company.name" />
+            </Datagrid>
+        </List>
+    );
+}
+
+export default UserList;
