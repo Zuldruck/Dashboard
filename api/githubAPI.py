1db66a2b5a317e162f088c6638fb1d780855fc4b
SimonProvost <simon1.provost@epitech.eu>
2019-11-07 23:48:26 +0100

    feat(simon): Github user info routes done.

--- api/githubAPI.py
+++ api/githubAPI.py
@@ -1,7 +1,3 @@
-import json
-import secrets
-
-import bcrypt
 import requests
 from flask import Blueprint, request, jsonify
 from requests import HTTPError
@@ -53,4 +49,45 @@ def getpopularRepositories():
                     }
                     repo.append(tmpDict)
                 return jsonify(repo)
+    return jsonify({"success": 404, "message": "Github problem occured.", "access_token": access_token})
+
+
+@github.route('/services/github/userInfo', methods=['POST'])
+def getuserInfo():
+    from index import db, user
+
+    access_token_github = request.json["accessTokenGithub"]
+    access_token = request.json["access_token"]
+
+    infoUser = []
+    all_users = db.child("users").get(user['idToken']).val()
+
+    for x in all_users:
+        if all_users[x]['access_token'] == access_token:
+
+            url = "https://api.github.com/user"
+            PARAMS = {
+                'Authorization': "token " + access_token_github,
+            }
+
+            try:
+                # sending get request and saving the response as response object
+                response = requests.get(url=url, headers=PARAMS)
+
+                # If the response was successful, no Exception will be raised
+                response.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            except Exception as err:
+                print(f'Other error occurred: {err}')  # Python 3.6
+            else:
+                infoUser = {
+                    "name": response.json()["name"],
+                    "company": response.json()["company"],
+                    "location": response.json()["location"],
+                    "bio": response.json()["bio"],
+                    "followers": response.json()["followers"],
+                    "following": response.json()["following"]
+                }
+            return jsonify(infoUser)
     return jsonify({"success": 404, "message": "Github problem occured.", "access_token": access_token})
